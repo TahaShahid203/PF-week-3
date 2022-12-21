e9e2f8a4d249f4f59f29a8d6f5f7dd64fae3ea7d
Taha Shahid <tahashahid203@gmail.com>
2022-12-21 14:06:36 +0500

    Week 3 Assignments

---
+++ Task01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+main() {
+int rollNumber;
+float aggregate;
+char section;
+string name;
+
+cout << "Enter your name: ";
+cin >> name;
+cout << "Enter your roll_number: ";
+cin >> rollNumber;
+
+cout << "Enter your aggregate: ";
+cin >> aggregate;
+
+cout << "Enter your section: ";
+cin >> section;
+
+cout << endl;
+cout << "Your name is: " << name << endl;
+cout << "Your roll number is: " << rollNumber << endl;
+cout << "Your section is: " << section << endl;
+cout << "Your aggregate is: " << aggregate << endl;
+
+
+
+
+}
